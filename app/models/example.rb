bf926593f7562802ad443a77e5b3bca8a6dfb40c
Max DePriest <maxd924@gmail.com>
2015-10-13 18:48:38 -0500

    shitty mongo_mapper documentation and 3 wasted hours later...finally got rails to connect to mongo

--- app/models/example.rb
+++ app/models/example.rb
@@ -1,2 +1,10 @@
-class Example < ActiveRecord::Base
+class Example
+	include MongoMapper::Document
+
+  key :nounOne, String, :required => true
+  key :nounTwo, String, :required => true
+  key :nounThree, String, :required => true
+  key :verbOne, String, :required => true
+  key :verbTwo, String, :required => true
+  key :verbThree, String, :required => true
 end
